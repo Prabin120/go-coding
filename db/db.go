c42b161c33f46cf22badea27495a2658a7ea1b5d
Prabin120 <87239578+Prabin120@users.noreply.github.com>
2024-11-26 17:11:44 +0000

    changing the env to read from .env

--- db/db.go
+++ db/db.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -21,9 +21,10 @@ var (
 // ConnectDB establishes a connection to MongoDB and returns a client.
 func ConnectDB() {
 	// Load environment variables from .env file
-	// if err := godotenv.Load(); err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	// Ignore while deploying in production
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(os.Getenv("MONGO_URI")).SetServerAPIOptions(serverAPI)
